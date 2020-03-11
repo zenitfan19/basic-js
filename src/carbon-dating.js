00dbdeab12011afaeaae3384033d89f1f6fb4e98
Andrey Lavrov <fanton96mailru@gmail.com>
2020-03-11 20:08:39 +0300

    feat: solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,10 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(sampleActivity === undefined || typeof sampleActivity !== "string" || !parseFloat(sampleActivity) || parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) >= MODERN_ACTIVITY) {
+    return false;
+  } else {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+  }
 };
