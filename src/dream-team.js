bc4dc065cdd8ee6ce53f6d63abe0623cb4a4ae14
Andrey Lavrov <fanton96mailru@gmail.com>
2020-03-11 20:48:09 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let secretName = "";
+  if(!members || !Array.isArray(members)) {
+    return false;
+  } else {
+    members.forEach(function(item) {
+      if(typeof item === "string") {        
+        secretName += item.replace(/\s/g, '')[0].toUpperCase();
+      }      
+    });
+    return secretName.split('').sort().join('');
+  }
 };
