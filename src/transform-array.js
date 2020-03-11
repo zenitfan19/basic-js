96755a68377b644ff9a792b6609a57bf97a70988
Andrey Lavrov <fanton96mailru@gmail.com>
2020-03-11 22:21:19 +0300

    feat: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,35 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let resultArr = [];
+  let missingNext = false;
+  if(!Array.isArray(arr)) {
+    throw 'Not array';
+  } else {
+    arr.forEach(function(item, i) {
+      switch(item) {
+        case '--discard-next':          
+          missingNext = true;
+          break;
+        case '--discard-prev':          
+          resultArr.pop();
+          break;
+        case '--double-next':          
+          if(arr[i+1] !== undefined) {
+            resultArr.push(arr[i+1]);
+          }          
+          break;
+        case '--double-prev':          
+          if(arr[i-1] !== undefined) {
+            resultArr.push(arr[i-1]);
+          }          
+          break;
+        default:
+          if(missingNext) {
+            missingNext = false;
+          } else {
+            resultArr.push(arr[i]);
+          }          
+      }
+    });
+  }
+  return resultArr;
 };
