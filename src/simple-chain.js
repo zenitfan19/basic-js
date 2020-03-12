e27cb9f9a3d18ea98e018cc3e80cef14ed6f5943
Andrey Lavrov <fanton96mailru@gmail.com>
2020-03-12 15:28:19 +0300

    feat: solve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,32 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;    
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if(arguments.length === 0) {
+      value = "";
+    }
+    this.chain.push('( ' + String(value) + ' )');
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if(Number.isInteger(position) && position > 0 && position <= this.chain.length) {
+      this.chain.splice(position-1, 1);
+      return this;
+    } else {
+      this.chain = [];
+      throw 'Invalid position';  
+    }    
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = this.chain.join('~~');
+    this.chain = [];
+    return result;
   }
 };
 
