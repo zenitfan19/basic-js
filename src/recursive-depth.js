48ff3e8bc5b44e2ca114f9cb1907505dee65cc4e
Andrey Lavrov <fanton96mailru@gmail.com>
2020-03-12 21:25:06 +0300

    feat: solve recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,27 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  constructor() {
+    this.depth = 0;
+  }
+  calculateDepth(arr, currentDepth) {
+    let curDepth = arguments.length === 2 ? currentDepth : 1;
+    this.depth = arguments.length === 2 ? this.depth : 1;    
+    if(!Array.isArray(arr)) {
+      throw 'Not an array';
+    } else if(arguments.length !== 2) {
+      this.depth = 1;
     }
+    if(arr.length !== 0) {
+      for(let element of arr) {        
+        if(Array.isArray(element)) {                    
+          this.depth = this.calculateDepth(element, curDepth+1);
+        } else {
+          this.depth = this.depth > curDepth ? this.depth : curDepth;
+        }
+      }
+    } else {
+      this.depth = this.depth > curDepth ? this.depth : curDepth;
+    }  
+        
+    return this.depth;
+  }
 };
